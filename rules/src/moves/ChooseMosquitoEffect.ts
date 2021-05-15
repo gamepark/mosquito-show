229f46c0ee1113360a00a99b4247bd4fee1b6356
Philipp Koppmann <philipp.koppmann@smeepit.de>
2021-05-15 21:48:23 +0200

    MoveType fixed

--- rules/src/moves/ChooseMosquitoEffect.ts
+++ rules/src/moves/ChooseMosquitoEffect.ts
@@ -4,7 +4,7 @@ import MoveType from "./MoveType"
  * A "Move" is the combination of all the types of moves that exists in you game
  */
 type ChooseMosquitoEffect = {
-    type: typeof MoveType.ChooseAnimal
+    type: typeof MoveType.ChooseMosquitoEffect
 }
 
 export default ChooseMosquitoEffect
