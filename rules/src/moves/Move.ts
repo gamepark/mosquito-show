139a3660f02bce83cfe006591ca09b7f5aaf0dba
Philipp Koppmann <philipp.koppmann@smeepit.de>
2021-09-17 21:59:22 +0200

    Move renamed

--- rules/src/moves/Move.ts
+++ rules/src/moves/Move.ts
@@ -2,11 +2,11 @@ import ChooseAnimal from "./ChooseAnimal"
 import ChooseMosquitoEffect from "./ChooseMosquitoEffect"
 import Eat from "./Eat"
 import MoveAnimal from "./MoveAnimal"
-import MosquitoEffect from "./PlayMosquitoEffect"
+import PlayMosquitoEffect from "./PlayMosquitoEffect"
 
 /**
  * A "Move" is the combination of all the types of moves that exists in you game
  */
-type Move = ChooseAnimal | Eat | MoveAnimal | MosquitoEffect | ChooseMosquitoEffect
+type Move = ChooseAnimal | Eat | MoveAnimal | PlayMosquitoEffect | ChooseMosquitoEffect
 
 export default Move
