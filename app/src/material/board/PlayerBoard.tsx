147593b1f965a4a49f0409beddae46799169ea25
Philipp Koppmann <philipp.koppmann@smeepit.de>
2021-11-13 22:33:35 +0100

    fill up the playerboards

--- app/src/material/board/PlayerBoard.tsx
+++ app/src/material/board/PlayerBoard.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { Mosquito } from '@gamepark/mosquito-show/material/MosquitoEffect'
 import PlayerColor from '@gamepark/mosquito-show/PlayerColor'
 import PlayerState from '@gamepark/mosquito-show/PlayerState'
 import { FunctionComponent, HTMLAttributes } from 'react'
-import { boardSize, headerHeight, margin } from '../../styles'
+import { boardSize, headerHeight, margin, mosquitoTokenSize, playerboardSize } from '../../styles'
 import MosquitoToken from '../fieldelement/MosquitoToken'
 
 type PlayerBoardProps = {
@@ -11,12 +12,17 @@ type PlayerBoardProps = {
   activePlayer?: PlayerColor
 } & HTMLAttributes<HTMLDivElement>
 
-const PlayerBoard: FunctionComponent<PlayerBoardProps> = ({playerstate, activePlayer, ...props}: PlayerBoardProps) => {
+const PlayerBoard: FunctionComponent<PlayerBoardProps> = ({ playerstate, activePlayer, ...props }: PlayerBoardProps) => {
   return <div css={outbox(playerstate.color, activePlayer)} {...props}>
-    {playerstate.eatenMosquitos.map( eatenMosquito =>
-      <MosquitoToken mosquito={eatenMosquito} css={tokenPosition}/>
+    {
+      [...Array(playerstate.goldenMosquitos)].map((_, index) =>
+        <MosquitoToken mosquito={Mosquito.Golden} css={goldenMosquitoPosition(index)} />
+      )
+    }
+    {playerstate.eatenMosquitos.map((eatenMosquito, index) =>
+      <MosquitoToken mosquito={eatenMosquito} css={eatenMosquitoPosition(index, playerstate.eatenMosquitos.length)} />
     )
-  }
+    }
   </div>
 }
 
@@ -24,17 +30,24 @@ export {
   PlayerBoard
 }
 
-const tokenPosition = css`
-  top: -10em;
-  left: -20em;
+const goldenMosquitoPosition = (index: number) => css`
+  position: absolute;
+  top: 10em;
+  left: ${1 + (index * ((playerboardSize - mosquitoTokenSize - 2) / 11))}em;
+`
+
+const eatenMosquitoPosition = (index: number, length: number) => css`
+  position: absolute;
+  top: 25em;
+  left: ${(playerboardSize - (length * mosquitoTokenSize)) / (length + 1) * (index + 1) + (index * mosquitoTokenSize)}em;
 `
 
 const outbox = (player: PlayerColor, activePlayer?: PlayerColor) => css`
   position: absolute;
   top: ${headerHeight + margin}em;
-  transform: translate(${(player === PlayerColor.Blue ? 1 : 1+(100*16/9-boardSize)/2 + boardSize)}em);
+  transform: translate(${(player === PlayerColor.Blue ? 1 : 1 + (100 * 16 / 9 - boardSize) / 2 + boardSize)}em);
   height: 45em;
-  width: ${(98*16/9-boardSize)/2}em;
-  border-style: ${player === activePlayer? 'dashed' : 'solid'};
+  width: ${playerboardSize}em;
+  border-style: ${player === activePlayer ? 'dashed' : 'solid'};
   border-color: ${(player === PlayerColor.Blue ? 'blue' : 'orange')};
 `
