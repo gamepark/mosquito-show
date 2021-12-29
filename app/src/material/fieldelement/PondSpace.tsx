014a504cab3b3d8303e5a420321b0efce83f519c
Philipp Koppmann <philipp.koppmann@smeepit.de>
2021-12-29 14:37:51 +0100

    glow for empty ponds on grey effect

--- app/src/material/fieldelement/PondSpace.tsx
+++ app/src/material/fieldelement/PondSpace.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
+import { css, keyframes } from '@emotion/react'
 import Animal from '@gamepark/mosquito-show/animals/Animal'
 import Coordinates from '@gamepark/mosquito-show/fields/Coordinates'
 import { Mosquito, MosquitoOnBoard } from '@gamepark/mosquito-show/material/MosquitoEffect'
@@ -39,14 +39,17 @@ export default function PondSpace({ game, x, y }: Props) {
   }
 
   const onPondSpaceClick = () => {
-    if (getActivePlayerState(game).selectedMosquito == Mosquito.Grey && mosquitos.length == 0 && (game.selectedPondSpace!.x != x || game.selectedPondSpace!.y != y)) {
+    if (getActivePlayerState(game).selectedMosquito == Mosquito.Grey && mosquitos.length == 0 && game.selectedPondSpace && (game.selectedPondSpace!.x != x || game.selectedPondSpace!.y != y)) {
       play(playGreyMosquitoEffectMove(game.selectedPondSpace!, { x, y }))
     }
     return undefined
   }
 
   return (
-    <div onClick={onPondSpaceClick} css={[style(x, y)]}>
+    <div onClick={onPondSpaceClick}
+      css={[style(x, y),
+      getActivePlayerState(game).selectedMosquito == Mosquito.Grey && mosquitos.length == 0 && emptyPondSpace,
+      getActivePlayerState(game).selectedMosquito == Mosquito.Grey && mosquitos.length == 0 && game.selectedPondSpace && glow]}>
       {mosquitos.map((mosquitoOnBoard, index) =>
         <MosquitoToken key={index} mosquito={mosquitoOnBoard.mosquito} waterlily={mosquitoOnBoard.waterlily} css={tokenPosition(index)}
           onClick={onMosquitoTokenClick(mosquitos.length === index + 1, mosquitoOnBoard)}
@@ -68,4 +71,32 @@ const style = (x: number, y: number) => css`
 const tokenPosition = (index: number) => css`
   top: ${index * -0.4}em;
   left: ${index * 0.4}em;
+`
+
+const emptyPondSpace = css`
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    border-radius: 50%;
+  }
+`
+
+const glowKeyframes = keyframes`
+  from {
+    box-shadow: 0 0 0.3em white, 0 0 0.3em white, 0 0 0.3em white;
+  }
+  to {
+    box-shadow: 0 0 0.6em white, 0 0 0.6em white, 0 0 0.6em white, 0 0 0.6em white, 0 0 0.6em white;
+  }
+`
+
+const glow = css`
+  cursor: pointer;
+  &:before, &:after {
+    animation: ${glowKeyframes} 2s infinite alternate ease-in-out;
+  }
 `
